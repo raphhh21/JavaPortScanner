22195fb452c0e7fdefd4db32d1cd58fdfc38cf87
raphhh21 <32976542+raphhh21@users.noreply.github.com>
2021-01-02 04:30:10 -0800

    Added open and close count in PortScannerResult

--- src/scanner/PortScannerResult.java
+++ src/scanner/PortScannerResult.java
@@ -24,18 +24,41 @@ public final class PortScannerResult {
     }
 
     private List<PortStatus> resultList;
+    private int openCount;
+    private int closeCount;
 
     public PortScannerResult() {
         resultList = new ArrayList<>();
+    }
+
+    /**
+     * Add a status of a port whether it is open or closed.
+     *
+     * @param portNum valid port number to add
+     * @param isOpen  boolean value to indicate if the port is open
+     */
+    public void addPortStatus(int portNum, boolean isOpen) {
+        PortStatus newStatus = new PortStatus(portNum, isOpen);
+        this.resultList.add(newStatus);
+
+        // count
+        if (isOpen) {
+            this.openCount++;
+        } else {
+            this.closeCount++;
+        }
     }
 
     public List<PortStatus> getResultList() {
         return this.resultList;
     }
 
-    public void addPortStatus(int portNum, boolean isOpen) {
-        PortStatus newStatus = new PortStatus(portNum, isOpen);
-        this.resultList.add(newStatus);
+    public int getOpenCount() {
+        return this.openCount;
+    }
+
+    public int getCloseCount() {
+        return this.closeCount;
     }
 
 }
